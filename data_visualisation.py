6502807dd93c368d3753c10cabb63768c943d9cf
diogojpa99 <diogo.jpa.99@gmail.com>
2022-10-05 02:04:36 +0100

    Data cleaning

--- data_visualisation.py
+++ data_visualisation.py
@@ -1,3 +1,4 @@
+from cv2 import threshold
 import pandas as pd
 import matplotlib.pyplot as plt
 import scipy.stats as stats
@@ -6,182 +7,145 @@ import numpy as np
 
 ''' Read the .csv file '''
 
-data = pd.read_csv('Proj1_Dataset.csv') 
-print(data.keys())
+df = pd.read_csv('Proj1_Dataset.csv') 
+print('Features: ', df.keys())
 
 
 ''' Initializations '''
 
-n_features = len(data.columns) ## Number of features
+n_features = len(df.columns) ## Number of features
 missing_data = [(-1,'Feature')] #We will not count the first position
+outliers =  [(-1,'Feature')] 
+
 
 
 ''' Functions '''
 
-# missing_data_detector():
+# missing_df_detector():
 # Returns an array with a vector og the shape:
-# (missing_data_index, Feature)
-def missig_data_detector(data_column, missing_data,feature): 
+# (missing_df_index, Feature)
+def missing_data_detector(data_column, missing_data,feature): 
     
     i = 0
     for d in data_column:
         if (d == True):
-            print('Missing Data index::',i)
+            #print('Missing df index::',i)
             missing_data.append((i,str(feature)))
         i += 1
     
     return
 
 
-# Plot - Observe the data 
-def plot_data(data):
-        
-    for i in range(2, len(data.columns)):
-        plt.plot(data[data.columns[i]])
-        plt.title(data.columns[i])
-        plt.show()
-        print(i,'Data:',data.columns[i])
-        i +=1
-        
-    return
-
-
-# Histogram - Observe the data 
-def plot_histogram(data):
+# Plot - Observe the df 
+def plot_df(df):
     
-    for i in range(2, len(data.columns)):
-        plt.hist(data[data.columns[i]]) # Histograms give more information about outliers
-        plt.title(data.columns[i])
-        plt.show()
-        i +=1
+    plt.ion()
+    fig, (ax1, ax2) = plt.subplots(1, 2)
         
+    for i in range(2, len(df.columns)):
+        
+        fig.suptitle(df.columns[i])
+        ax1.plot(df[df.columns[i]])
+        ax2.hist(df[df.columns[i]]) # Histograms give more information about outliers  
+        ax2.set_title('Histogram')      
+        plt.show()
+        plt.pause(4)
+        ax1.cla()
+        ax2.cla()
+    
+    fig.clf()
     return
 
 
 # Observe if there is correlation between different features 
-def plot_correlation(data):
+def plot_correlation(df):
 
     print('Please insert the index of the first feature:')
     feature_1 = int(input())
     print('Please insert the index of the second feature:')
     feature_2 = int(input())
-
-    plt.scatter(data[data.columns[feature_1]], data[data.columns[feature_2]])
-    plt.title("Correlation between two variables")
-    plt.show()
     
+    fig, ax = plt.subplots()
+
+    scatter = ax.scatter(df[df.columns[feature_1]], df[df.columns[feature_2]], c = df['Persons'], s = 10)
+
+    # produce a legend with the unique colors from the scatter
+    legend1 = ax.legend(*scatter.legend_elements(), loc="upper left", title="Persons")
+    ax.add_artist(legend1)
+    ax.set_xlabel(df.columns[feature_1])
+    ax.set_ylabel(df.columns[feature_2])
+    fig.suptitle("Correlation between two Features")
+    plt.show()
+  
     return
 
 
 ''' main() '''
 
-###### Missing Data Detection #######
+###### Missing df Detection #######
 
 for i in range (2,n_features):
-    print('---------------------------')
-    print('Feature: ', data.columns[i])
-    missig_data_detector(data.isnull()[data.columns[i]], missing_data, data.columns[i])
+    #print('---------------------------')
+    #print('Feature: ', df.columns[i])
+    missing_data_detector(df.isnull()[df.columns[i]], missing_data, df.columns[i])
     
-print("Missing Data Index", missing_data)
+missing_data.pop(0)
+print('-----------------------------------------------------------')
+print("Missing data Index", missing_data)
+print('-----------------------------------------------------------')
 
+###### Filling Missing df ######
 
-###### Filling Missing Data ######
+print('-----------------------------------------------------------')
+print('Select the mode you want for filling missing df:')
+print('-----------------------------------------------------------')
+print('1. Interpolation of the previous and the next value')
+print('-----------------------------------------------------------')
+interpolation_flag = int(input())
 
-print('Select the mode you want for filling missing data:')
-print('Interpolation of the previous and the next one: 1')
-missing_data_flag = int(input())
-
-if missing_data_flag == 1:   
+if interpolation_flag == 1:   
     for m in missing_data:
-        if m[0] != -1:
-            data[m[1]][m[0]] = (data[m[1]][m[0]-1]+ data[m[1]][m[0]+1])/2
+        df.loc[m[0],m[1]] = (df.loc[m[0]-1,m[1]] + df.loc[m[0]+1,m[1]])/2
     
 
 
 ###### Outlier Detection #######
 
-outliers = [-1] #We will not count the first position
+##### Z-score ######
 
-def outlier_detector_stdev(feature, stdev, outliers): 
-   
-    stdev = feature.std()
-    avg = feature.mean()
-    k = 4
-    i = 0
+def z_score(df_column,feature,outliers): 
     
-    for f in feature:
-        if (f > avg + k*stdev) or (f < avg - k*stdev):
-            print(i, 'Outlier')
-            outliers.append(i)
-        i += 1
-        
-    return
-
-''' Z-Score '''
-
-def z_score(feature): 
-   
-    stdev = feature.std()
-    avg = feature.mean()
+    df_column = np.array(df_column)
+    stdev = df_column.std()
+    avg = df_column.mean()
     column = [-1]
+    threshold = 7.5
 
-    for f in feature:
-        column.append(abs(f-avg)/stdev)
+    for d in df_column:
+        column.append(abs(d-avg)/stdev)
     
-    column.pop(0) #remove first element of list
+    column.pop(0) #Remove first element of list
     column = np.array(column)
     
-    print(column)
-    print(column.mean())
-    print(column.max())
-    
-    return column
+    for i in range(len(column)):
+        if (column[i] > threshold*column.mean()):
+            outliers.append((i,str(feature)))
 
-import seaborn as sns
+    return 
 
-st_column = z_score(data[data.columns[2]])
+for i in range (2,n_features):
+    z_score(df[df.columns[i]],df.columns[i],outliers)
 
-exit(0)
+outliers.pop(0)
+print('-----------------------------------------------------------')
+print('Outliers',outliers)
+print('-----------------------------------------------------------')
 
-    
-data_values = data.drop(['Date','Time'], axis = 1)
-print(data_values.apply(stats.zscore))
-print(type(data_values.apply(stats.zscore)))
+#plot_df(df)
 
-
-for i in range(2,n_features):
-    sns.histplot(data.apply(stats.zscore))
-    plt.show()
-    
-"""for i in range (2,n_features):
-    #outlier_detector_stdev(data[data.columns[i]],stdev, outliers)
-    outlier_detector_quantiles(data[data.columns[i]], quantiles, outliers)"""
-
-print('Insert Feature:')
-outlier_flag = int(input())
-outlier_detector_stdev(data[data.columns[outlier_flag]],stdev, outliers)
-print(outliers)
-
-exit(0)
-
-
-
-
-print("New")
-print(data[missing_data[1][1]][missing_data[1][0]-1])   
-print(data[missing_data[1][1]][missing_data[1][0]])    
-print(data[missing_data[1][1]][missing_data[1][0]+1])        
-
-f= 0
-for i in data['S1Temp']:
-    if i <15:
-        print(f,i) 
-    f +=1 
-          
-plt.plot(data['S1Temp'])
-plt.title('S1Temp')
-plt.show()
-                
-plt.hist(data['S1Temp']) # Histograms give more information about outliers
-plt.title('S1Temp')
-plt.show()
+if interpolation_flag == 1:  
+    for o in outliers:
+        df.loc[o[0],o[1]] = (df.loc[o[0]-1,o[1]] + df.loc[o[0]+1,o[1]])/2
+        
+#plot_df(df)
+plot_correlation(df)
