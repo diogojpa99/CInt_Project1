502ae5389dbb627e2efb2b823658ba4bd31b5798
diogojpa99 <diogo.jpa.99@gmail.com>
2022-10-16 16:36:08 +0100

    FInnish Fuzzy

--- part2/part2.py
+++ part2/part2.py
@@ -8,12 +8,11 @@ from sklearn.preprocessing import MinMaxScaler
 from imblearn.pipeline import Pipeline as imbpipeline
 from sklearn.pipeline import Pipeline
 from sklearn.neural_network import MLPClassifier
-import sklearn.metrics as met
 from sklearn.metrics import precision_score, recall_score,f1_score, balanced_accuracy_score, accuracy_score
 from scipy.stats import pearsonr
 from sklearn.metrics import plot_confusion_matrix
 from sklearn.metrics import classification_report
-import pickle
+import sklearn.metrics as met
 import math
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
@@ -24,13 +23,13 @@ from skfuzzy import control as ctrl
 df = pd.read_csv('Proj1_Dataset.csv') 
 print('Features: ', df.keys())
 
-#sns.set_theme(style="darkgrid")
+sns.set_theme(style="darkgrid")
 
 n_features = len(df.columns) ## Number of features
 missing_data = [(-1,'Feature')] #We will not count the first position
 outliers =  [(-1,'Feature')] 
 
-sm = True #SMOTE
+sm = False #SMOTE
 
 """*********************************  Functions() ************************************"""
 
@@ -141,21 +140,6 @@ def Normalize_data(data, train_set):
     
     return scaler.transform(data)
 
-def Save_Max_Min(df_train):
-    
-    df_norm = {'S1Temp':[1,0],'S2Temp':[1,0],'S3Temp':[1,0],
-               'S1Light':[1,0],'S2Light':[1,0],'S3Light':[1,0],
-               'CO2':[1,0],'PIR1':[1,0],'PIR2':[1,0]}
-    
-    
-    return
-
-#Balance Data with SMOTE method
-def Balance_data(x_train, y_train):
-    
-    sm = SMOTE(random_state = 11)
-    return sm.fit_resample(x_train,y_train)
-
 #Feature Selection
 # If two features have a pearson correlation coefficient bigger than 85 remove one of them
 def Feature_Selection(df):
@@ -180,13 +164,10 @@ def Feature_Selection(df):
 
 #Print scores
 def Print_Scores_binary(Y_pred,Y_test):
-    
-    print('binary - Accuracy:', accuracy_score(Y_pred,Y_test))
-    print('binary - Balanced Accuracy:', balanced_accuracy_score(Y_pred,Y_test))
-    
-    print('binary - Precision:', precision_score(Y_pred,Y_test, average='binary'))
-    print('binary - Recall:', recall_score(Y_pred,Y_test, average='binary'))
-    print('binary - f1-score:', f1_score(Y_pred,Y_test, average='binary'))
+        
+    print('Binary - Precision:', precision_score(Y_pred,Y_test, average='binary'))
+    print('Binary - Recall:', recall_score(Y_pred,Y_test, average='binary'))
+    print('Binary - f1-score:', f1_score(Y_pred,Y_test, average='binary'))
     
     return
 
@@ -206,15 +187,15 @@ def model_FineTuning(X_train, Y_train, sm ):
         
     stratified_kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=11)
          
-    param_grid = {'MLP__hidden_layer_sizes':[(12,12),(14,)], 
+    param_grid = {'MLP__hidden_layer_sizes':[(6,),(5,5)], 
                   'MLP__activation':['tanh','relu'], 
                   'MLP__solver':['adam'],
                   'MLP__alpha':alphas, 
                   'MLP__learning_rate':['constant','adaptive'],
-                  'MLP__max_iter':[500],
+                  'MLP__max_iter':[1000],
                   'MLP__learning_rate_init':l_rates}
     
-    #scoring = {'accuracy':met.make_scorer(met.accuracy_score)}
+    scoring = {'accuracy':met.make_scorer(met.accuracy_score)}
     grid_search = GridSearchCV(estimator=pipeline, param_grid=param_grid, scoring='roc_auc',
                                cv=stratified_kfold)
     
@@ -223,6 +204,20 @@ def model_FineTuning(X_train, Y_train, sm ):
     print(grid_search.best_score_)
 
     return
+
+# According to model()
+def MLP_binary_model(X_train, Y_train, X_test, Y_test):
+    
+    mlp = MLPClassifier(hidden_layer_sizes=(6,), activation='tanh',
+                        solver = 'adam', alpha = 0.0001, learning_rate='constant', 
+                        max_iter = 1000, learning_rate_init = 0.01225).fit(X_train,Y_train)
+    
+    Y_pred = mlp.predict(X_test)
+    
+    Print_Scores_binary(Y_pred,Y_test)
+
+    return
+
 
 #Plotonfusion Matrix
 def Plot_ConfusionMatrix(mlp, test_x, test_y, pred_y):
@@ -246,6 +241,53 @@ def Plot_LossCurve(mlp):
     plt.close()
     
     return
+    
+    
+def Tune_fuzzy_classifier(x_train, y_train, nr_persons_ctrl):
+    
+    y_pred_train = np.array([])
+    
+    for i in range(len(x_train)):
+        nr_persons_ctrl.input['Temp'] = x_train[i,0]
+        nr_persons_ctrl.input['Lights_Avg'] = x_train[i,1]
+        nr_persons_ctrl.input['CO2_Var'] = x_train[i,2]
+        nr_persons_ctrl.compute()
+        y_pred_train = np.append(y_pred_train, math.ceil(nr_persons_ctrl.output['Persons']))
+        
+    for i in range (len(y_pred_train)):
+        if y_pred_train[i] == 3:
+            y_pred_train[i] = 1
+        else:
+            y_pred_train[i] = 0
+
+    Print_Scores_binary(y_pred_train,y_train)
+    
+    return
+    
+    
+def Fuzzy_classifier(x_test, y_test, nr_persons_ctrl):
+    
+    y_pred = np.array([])
+
+
+    for i in range(len(x_test)):
+        nr_persons_ctrl.input['Temp'] = x_test[i,0]
+        nr_persons_ctrl.input['Lights_Avg'] = x_test[i,1]
+        nr_persons_ctrl.input['CO2_Var'] = x_test[i,2]
+        nr_persons_ctrl.compute()
+        y_pred = np.append(y_pred, math.ceil(nr_persons_ctrl.output['Persons']))
+        
+        
+    for i in range (len(y_pred)): #Pass for binary
+        if y_pred[i] == 3:
+            y_pred[i] = 1
+        else:
+            y_pred[i] = 0
+            
+    Print_Scores_binary(y_pred,y_test)
+
+    return
+
     
 """*********************************  main() ************************************"""
 
@@ -271,15 +313,13 @@ outliers.pop(0)
 
 """********************* Removing Outliers **********************"""
 
-df = Filling_data(df, outliers)
-
 df_fuzzy = Filling_data(df, outliers)
 
 """**************************** Fuzzy - Define Variables ***********************************"""
 
 ####### S1Temp #######
 
-''' Não vamos alterar a S1Temp'''
+''' S1Temp Will Stay the same '''
 
 '''sns.relplot(data = df, x = df['Time'],y = df['S1Temp'],hue="Persons")
 plt.show()'''
@@ -287,24 +327,23 @@ plt.show()'''
 min_temp = min(df_fuzzy['S1Temp'])
 max_temp = max(df_fuzzy['S1Temp'])
 
-####### CO2 #######
+####### CO2 Var #######
+
+''' Variation between CO2 instances '''
 
 df_fuzzy['CO2'] = np.append(np.diff(df_fuzzy['CO2']),0)
 
 max_CO2 = max(df_fuzzy['CO2'])
 min_CO2 = min(df_fuzzy['CO2'])
 
-    
 ####### Light Avg #######
+
+''' Mean of all the three SLight Sensors '''
 
 df_fuzzy['S1Light'] = (df_fuzzy['S1Light']+df_fuzzy['S2Light']+df_fuzzy['S3Light'])/3
 
 max_light_avg = max(df_fuzzy['S1Light'])
 min_light_avg = min(df_fuzzy['S1Light'])
-
-plt.plot(df_fuzzy['S1Light'])
-plt.title('Average of the three light sensors')
-plt.show()
 
 ####### Data ########
 
@@ -320,22 +359,27 @@ x_train, x_test, y_train, y_test = train_test_split(df_fuzzy.iloc[:,:(len(df_fuz
                                                     df_fuzzy.iloc[:,(len(df_fuzzy.columns)-1)].values,
                                                     test_size=0.1,shuffle=True)
 
-
 """*********** [Training data] Dealing with noise - Moving Average ***************"""
 
 # train_set = Clean_Noise(train_set)
 
 """************************* Model Fine-Tunning ******************************"""
 
-#model_FineTuning(x_train, y_train, True) #To do Grid_Seacrh CV with SMOTE
+for i in range (len(y_train)):
+    if y_train[i] == 3:
+        y_train[i] = 1
+    else:
+        y_train[i] = 0
+        
+#model_FineTuning(x_train, y_train, False) #To do Grid_Seacrh CV without SMOTE
 
 """********************* Normalize the Training Set *************************"""
 
-#x_train_norm = Normalize_data(x_train, x_train)
+x_train_norm = Normalize_data(x_train, x_train)
 
 """********************* Normalize the Test Set *************************"""
 
-#x_test_norm = Normalize_data(x_test, x_train)
+x_test_norm = Normalize_data(x_test, x_train)
 
 """********************* Balance the Training Set ***************************"""
 
@@ -357,16 +401,16 @@ Persons = ctrl.Consequent(np.arange(0, 3+1, 1), 'Persons')
 
 ###### Input ########
 
-temp['cold'] = fuzz.trimf(temp.universe,[min_temp, min_temp ,20.5])
-temp['hot'] = fuzz.trimf(temp.universe,[20.1, 21 , max_temp])
+temp['cold'] = fuzz.trimf(temp.universe,[min_temp, 20.1 ,20.3])
+temp['hot'] = fuzz.trapmf(temp.universe,[20.0, 20.8 , max_temp-0.1, max_temp])
 
 mean_lights['low'] = fuzz.trimf(mean_lights.universe,[min_light_avg, min_light_avg, 180])
 mean_lights['medium'] = fuzz.trimf(mean_lights.universe,[140, 190, 375])
-mean_lights['high'] = fuzz.trimf(mean_lights.universe,[345, max_light_avg, max_light_avg]) 
+mean_lights['high'] = fuzz.trimf(mean_lights.universe,[330, max_light_avg, max_light_avg]) 
 
 CO2_deriv['decrease'] = fuzz.trimf(CO2_deriv.universe, [min_CO2, min_CO2, -5]) 
-CO2_deriv['constant'] = fuzz.trimf(CO2_deriv.universe, [-10, 0, 15])
-CO2_deriv['increase'] = fuzz.trimf(CO2_deriv.universe, [5, max_CO2, max_CO2])
+CO2_deriv['constant'] = fuzz.trimf(CO2_deriv.universe, [-15, 0, 15])
+CO2_deriv['increase'] = fuzz.trapmf(CO2_deriv.universe, [5, 30, max_CO2, max_CO2])
 
 
 #Plots
@@ -399,16 +443,16 @@ rule8 = ctrl.Rule(mean_lights['high'] & CO2_deriv['constant'], Persons['EqualToT
 rule9 = ctrl.Rule(mean_lights['high'] & CO2_deriv['increase'], Persons['EqualToThree'])
 
 rule10 = ctrl.Rule(temp['cold'] & CO2_deriv['decrease'], Persons['LowerThanThree'])
-rule11= ctrl.Rule(temp['cold'] & CO2_deriv['constant'], Persons['LowerThanThree'])
+rule11 = ctrl.Rule(temp['cold'] & CO2_deriv['constant'], Persons['LowerThanThree'])
 rule12 = ctrl.Rule(temp['cold'] & CO2_deriv['increase'], Persons['LowerThanThree'])
 
 rule13 = ctrl.Rule(temp['hot'] & CO2_deriv['decrease'], Persons['LowerThanThree'])
-rule14 = ctrl.Rule(temp['hot'] & CO2_deriv['constant'], Persons['LowerThanThree'])
+rule14 = ctrl.Rule(temp['hot'] & CO2_deriv['constant'], Persons['EqualToThree'])
 rule15 = ctrl.Rule(temp['hot'] & CO2_deriv['increase'], Persons['EqualToThree'])
 
 rule16 = ctrl.Rule(temp['cold'] & mean_lights['low'], Persons['LowerThanThree'])
 rule17 = ctrl.Rule(temp['cold'] & mean_lights['medium'], Persons['LowerThanThree'])
-rule18 = ctrl.Rule(temp['cold'] & mean_lights['high'], Persons['EqualToThree'])
+rule18 = ctrl.Rule(temp['cold'] & mean_lights['high'], Persons['LowerThanThree'])
 
 rule19 = ctrl.Rule(temp['hot'] & mean_lights['low'], Persons['LowerThanThree'])
 rule20 = ctrl.Rule(temp['hot'] & mean_lights['medium'], Persons['LowerThanThree'])
@@ -417,59 +461,29 @@ rule21 = ctrl.Rule(temp['hot'] & mean_lights['high'], Persons['EqualToThree'])
 """*********************************** Inference Engine ***************************************"""
 
 # Control System
-persons_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, 
-                                   rule9,rule10,rule11,rule15,rule16,rule17,rule18,rule20,rule21])
+"""persons_ctrl = ctrl.ControlSystem([rule1, rule2, rule3, rule4, rule5, rule6, rule7, rule8, 
+                                   rule9,rule10,rule11,rule12,rule13,rule14,rule15,rule16,rule17,rule18,rule20,rule21])"""
 
-# Control System Simulation
-nr_persons_ctrl = ctrl.ControlSystemSimulation(persons_ctrl)
+persons_ctrl = ctrl.ControlSystem([rule1, rule2, rule6, rule8, rule9,rule10,rule11,
+                                   rule12,rule14, rule15,rule17,rule18,rule20,rule21])
 
 """*********************************** Defuzzifier ***************************************"""
 
-y_pred = np.array([])
-y_pred_train = np.array([])
-
-for i in range(len(x_test)):
-    nr_persons_ctrl.input['Temp'] = x_test[i,0]
-    nr_persons_ctrl.input['Lights_Avg'] = x_test[i,1]
-    nr_persons_ctrl.input['CO2_Var'] = x_test[i,2]
-    nr_persons_ctrl.compute()
-    y_pred = np.append(y_pred, math.ceil(nr_persons_ctrl.output['Persons']))
-    
-for i in range(len(x_train)):
-    nr_persons_ctrl.input['Temp'] = x_train[i,0]
-    nr_persons_ctrl.input['Lights_Avg'] = x_train[i,1]
-    nr_persons_ctrl.input['CO2_Var'] = x_train[i,2]
-    nr_persons_ctrl.compute()
-    y_pred_train = np.append(y_pred_train, math.ceil(nr_persons_ctrl.output['Persons']))
-    
-    
-    
-        
+# Control System Simulation
+nr_persons_ctrl = ctrl.ControlSystemSimulation(persons_ctrl)
+       
 """*********************************** Output ***************************************"""
 
-for i in range (len(y_pred)):
-    if y_pred[i] == 3:
-        y_pred[i] = 1
-    else:
-        y_pred[i] = 0
-        
+#Tune_fuzzy_classifier(x_train, y_train,nr_persons_ctrl)
+
+for i in range (len(y_test)): #Pass for binary        
     if y_test[i] == 3:
         y_test[i] = 1
     else:
         y_test[i] = 0
+
+print("------------- Fuzzy Output -------------------")
+Fuzzy_classifier(x_test, y_test, nr_persons_ctrl)
         
-for i in range (len(y_pred_train)):
-    if y_train[i] == 3:
-        y_train[i] = 1
-    else:
-        y_train[i] = 0
-        
-
-    if y_pred_train[i] == 3:
-        y_pred_train[i] = 1
-    else:
-        y_pred_train[i] = 0
-
-
-Print_Scores_binary(y_pred,y_test)
-Print_Scores_binary(y_pred_train,y_train)
+print("--------------- MLP Output -------------------")
+MLP_binary_model(x_train_norm, y_train, x_test_norm, y_test)
